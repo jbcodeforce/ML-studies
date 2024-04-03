c10a91e52fbe46221917c3058570fdee91357ffe
jbcodeforce <jerome.boyer@gmail.com>
2024-04-02 18:17:19 -0700

    add watson

---
+++ docs/techno/watsonx.md
@@ -0,0 +1,12 @@
+# WatsonX.ai
+
+WatsonX.ai offers a set of features to use LLM as APIs. 
+
+Once IBM Cloud account is created, we need to also sign-up to WatsonX.ai.
+
+Once done a sandbox project is created, we need to get the project ID using the Info menu in the project page.
+
+![](./images/watsonx-project.png)
+
+* Create an IBM API KEY with Manage (in top menu bar) > Access (IAM) > API keys.
+to start using Watson with [LangChain chain](https://python.langchain.com/docs/integrations/llms/ibm_watsonx)
--- llm-langchain/watsonX/TestCallToOneLLM.py
+++ llm-langchain/watsonX/TestCallToOneLLM.py
@@ -1,19 +1,105 @@
-import requests,os
+import requests,os,sys
 from dotenv import load_dotenv
+from langchain_ibm import WatsonxLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-load_dotenv()
+load_dotenv(dotenv_path="../.env")
+try:
+    api_key=os.environ.get("IBM_API_KEY")
+    project_id=os.environ.get("IBM_WATSON_PROJECT_ID")
+    watson_api_key=os.environ.get("WATSONX_APIKEY")
+except KeyError:
+    print(" Set the IBM_API_KEY environment variable")
+    sys.exit(1)
 
-api_key= os.environ["IBM_API_KEY"]
-
+print(api_key)
 # Get an IAM token from IBM Cloud
 url     = "https://iam.cloud.ibm.com/identity/token"
 headers = { "Content-Type" : "application/x-www-form-urlencoded" }
-data    = "apikey=" + api_key + "&grant_type=urn:ibm:params:oauth:grant-type:apikey"
-response  = requests.post( url, headers=headers, data=data, auth=api_key )
+data    =  "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey="+api_key
+
+response  = requests.post( url, headers=headers, data=data)
 iam_token = response.json()["access_token"]
 
 print(iam_token)
+
 from ibm_watsonx_ai.foundation_models.utils.enums import ModelTypes
 import json
 
 print( json.dumps( ModelTypes._member_names_, indent=2 ) )
+
+
+parameters = {
+            "decoding_method": "sample",
+            "max_new_tokens": 200,
+            "min_new_tokens": 1,
+            "temperature": 0.5,
+            "top_k": 50,
+            "top_p": 1,
+        }
+
+llm = WatsonxLLM(
+            model_id="ibm/granite-13b-instruct-v2",
+            url="https://us-south.ml.cloud.ibm.com",
+            project_id=project_id,
+            params=parameters,
+            api_key=watson_api_key
+        )
+
+print(llm.invoke("Who is man's best friend?"))
+
+
+url = "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29"
+
+body = {
+	"input": """As technical writer can you improve the following sentence:
+
+I will take a imaginary business case of car ride on autonomous vehicles.
+
+For this discussion, the simplest requirements include, booking a ride to go from one location to another location, manage a fleet of autonomous cars, get payment from the customer once the trip is done, get estimation for the ride duration and price to present a proposition to the end-users.
+
+The modern synchronous based microservice component view may look like in the following figure:""",
+	"parameters": {
+		"decoding_method": "greedy",
+		"max_new_tokens": 200,
+		"repetition_penalty": 1
+	},
+	"model_id": "ibm/granite-13b-chat-v2",
+	"project_id": "e44c2b60-dc53-4e6d-99a0-cf5ca013d17b",
+	"moderations": {
+		"hap": {
+			"input": {
+				"enabled": true,
+				"threshold": 0.5,
+				"mask": {
+					"remove_entity_value": true
+				}
+			},
+			"output": {
+				"enabled": true,
+				"threshold": 0.5,
+				"mask": {
+					"remove_entity_value": true
+				}
+			}
+		}
+	}
+}
+
+headers = {
+	"Accept": "application/json",
+	"Content-Type": "application/json",
+	"Authorization": "Bearer YOUR_ACCESS_TOKEN"
+}
+
+response = requests.post(
+	url,
+	headers=headers,
+	json=body
+)
+
+if response.status_code != 200:
+	raise Exception("Non-200 response: " + str(response.text))
+
+data = response.json()
