5bf5cfe04aa503df3c90d70fd0330735fe2cc6eb
jbcodeforce <jboyer@confluent.io>
2026-01-04 19:36:17 -0800

    update the shell to get key from env var

--- e2e-demos/claude_code_liteLLM/main.py
+++ e2e-demos/claude_code_liteLLM/main.py
@@ -1,3 +1,10 @@
+import os
+import litellm
+
+"""
+Implement with litellm sdk
+"""
+
 def main():
     print("Hello from claude-code-litellm!")
 
--- e2e-demos/claude_code_liteLLM/test_litellm_ollama.sh
+++ e2e-demos/claude_code_liteLLM/test_litellm_ollama.sh
@@ -1,6 +1,7 @@
 curl -X POST 'http://0.0.0.0:4000/chat/completions' \
 -H 'Content-Type: application/json' \
--H 'Authorization: Bearer $LITELLM_DEV_KEY' -d '{ "model": "ollama-gpt-oss", "messages": [
+-H "Authorization: Bearer $LITELLM_DEV_KEY" \
+-d '{ "model": "ollama-gpt-oss", "messages": [
       {
         "role": "system",
         "content": "You are a helpful math tutor. Guide the user through the solution step by step."
