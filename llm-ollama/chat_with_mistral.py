45e872e047a778d35321de96d61f650b04246b76
jbcodeforce <jerome.boyer@gmail.com>
2026-01-04 21:01:01 -0800

    update chat with olama

--- llm-ollama/chat_with_mistral.py
+++ llm-ollama/chat_with_mistral.py
@@ -4,21 +4,24 @@ from pydantic import BaseModel
 import os
 
 mistral_model_name=os.getenv("LLM_MODEL","mistral-small:latest")
-agent_system_prompt = "You are an expert in AI, answer the user question."
+agent_system_prompt = "You are an expert in game coding and implementation  answer the user question."
 
 def chat_with_ollama(question: str) -> str:
     messages = [
       {"role": "system", "content": agent_system_prompt},
        {"role": "user", "content": f"question: {question}" }
     ]
-
+    result= ""
     for part in chat(model=mistral_model_name,
                  messages=messages, 
                  options={'temperature': 0.2},
                  stream=True,
                  # think=True  -- only available for specific models
                 ):
-        print(part['message']['content'], end='', flush=True)
+        result+=part['message']['content']
+        print(part['message']['content'])
+    return result
+
 
 if __name__ == '__main__':
     print("Chat with mistral until entering an empty question")
