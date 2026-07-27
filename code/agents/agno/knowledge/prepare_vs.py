2d0679ba753388c391abfeba5ef19364c1aeb03a
jbcodeforce <jboyer@confluent.io>
2026-07-27 11:49:39 -0700

    update to km agno

--- code/agents/agno/knowledge/prepare_vs.py
+++ code/agents/agno/knowledge/prepare_vs.py
@@ -9,7 +9,8 @@ from agno.knowledge.knowledge import Knowledge
 from agno.knowledge.embedder.fastembed import FastEmbedEmbedder
 from datetime import datetime
 from pydantic import BaseModel
-from typing import List
+from agno.knowledge.remote_content import GitHubConfig
+from typing import List, Optional
 import json
 import hashlib
 from dotenv import load_dotenv
@@ -27,12 +28,19 @@ collection_name = "astronomy"
 class DocumentMetadata(BaseModel):
     name: str
     path: str
-    url: str
+    url: Optional[str] = None
     src_type: str
-    sha256: str
+    sha256: Optional[str] = None
     processed: bool
-    updated_at: str
+    updated_at: Optional[str] = None
     metadata: dict
+
+github_config = GitHubConfig(
+    id="dynamic-repo",
+    name="Dynamic GitHub Source",
+    token=os.getenv("GITHUB_TOKEN"),
+    branch="main",
+)
 
 def prepare_knowledge_base() -> Knowledge:
     """
@@ -48,6 +56,7 @@ def prepare_knowledge_base() -> Knowledge:
     )
     knowledge_base = Knowledge(
         vector_db=vector_db,
+        content_sources=[github_config]
     )
     return knowledge_base
 
@@ -60,13 +69,20 @@ def load_documents_from_manifest(manifest_file: str) -> List[DocumentMetadata]:
         data = json.load(f)
     return [DocumentMetadata.model_validate(item) for item in data]
 
-def update_manifest(manifest_file, document: DocumentMetadata, absolute_path: str, manifests: List[DocumentMetadata]):
+def update_manifest(manifest_file, 
+                    document: DocumentMetadata, 
+                    absolute_path: str, 
+                    manifests: List[DocumentMetadata],
+                    remote_content: Optional[str] = None):
     """
     Update the manifest file with the document metadata.
     """
     document.processed = True
     document.updated_at = datetime.now().isoformat()
-    document.sha256 = hashlib.sha256(open(absolute_path, "r").read().encode()).hexdigest()
+    if document.src_type == "local_file":   
+        document.sha256 = hashlib.sha256(open(absolute_path, "r").read().encode()).hexdigest()
+    elif document.src_type == "github":
+        document.sha256 = hashlib.sha256(remote_content.content.encode()).hexdigest()
     with open(manifest_file, "w") as f:
         json.dump([document.model_dump() for document in manifests], f)
 
@@ -86,9 +102,19 @@ def process_manifests(kb: Knowledge, current_dir: str):
             continue
         absolute_path = str(current_dir) + '/test_docs/' + document.path
         print(f"Process document: {document.name} in {absolute_path} with metadata: {document.metadata}")
-        kb.insert(name=document.name, 
-                  path=absolute_path, 
-                  metadata=document.metadata)
+        if document.src_type == "local_file":
+            kb.insert(name=document.name, 
+                      path=absolute_path, 
+                      metadata=document.metadata)
+        elif document.src_type == "github":
+            remote_content = github_config.file(document.path, repo=document.url)
+            if remote_content is None:
+                print(f"Remote content not found for {document.name} in {document.url}")
+                continue
+            print(remote_content.content)
+            kb.insert(name=document.name, 
+                      remote_content=remote_content.content,
+                      metadata=document.metadata)
         update_manifest(manifest_file, document, absolute_path, manifest)
 
 if __name__ == "__main__":
